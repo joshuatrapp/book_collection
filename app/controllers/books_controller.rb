dfbd9533600b0342bde7de11e94b029bce376eaa
root <root@localhost>
2025-09-09 16:32:04 +0000

    part 2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -47,7 +47,7 @@ class BooksController < ApplicationController
     private
 
     def book_params
-        params.require(:book).permit(:title)
+        params.require(:book).permit(:title, :author, :price, :published_date)
     end
 
 end
