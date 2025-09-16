cf7bbff49c80dcfa4a996aa12347a41c9dc6e8dd
root <root@localhost>
2025-09-16 00:10:38 +0000

    part4: associations

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  resources :user_books
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "user_books#index"
 
   resources :books do
     member do
