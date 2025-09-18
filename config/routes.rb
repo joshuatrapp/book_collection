a27c0b7f191d519023e69a394ed8c5d624abae7d
trapp <joshuatrapp@tamu.edu>
2025-09-18 03:34:55 +0000

    heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   resources :user_books
   resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
   root "user_books#index"
 
   resources :books do
